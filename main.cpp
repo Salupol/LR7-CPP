42ed144a19d9f6f3bbd2dbbfa152c19fb5792f01
Pavlo <44544644+Salupol@users.noreply.github.com>
2024-04-12 08:55:54 +0300

    3

--- main.cpp
+++ main.cpp
@@ -1,106 +1,47 @@
-#include <iostream>
-#include <fstream>
-#include <string>
 #include <cctype>
-#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
-bool hasPunct(const string &s) {
-    for (char c: s) {
-        if (c == '.' || c == ',' || c == '?' || c == ';') {
-            return true;
-        }
-
-    }
-    return false;
-}
-
-bool wordisPunct(const string &s) {
-    if (s == "." || s == "," || s == "?" || s == ";") {
-        return true;
-    }
-    return false;
-}
-
-void processWord(string &word, fstream &f2) {
-    if (hasPunct(word) && word[word.length() - 1] != '.' && word[word.length() - 1] != ',' && word[word.length() - 1] != '?' && word[word.length() - 1] != ';') {
-        for (int i = 0; i < word.length(); i++) {
-            if (word[i] == '.' || word[i] == ',' || word[i] == '?' || word[i] == ';') {
-                word.replace(i, 1, " ");
-                stringstream ss(word);
-                string word1;
-                while (getline(ss, word1, ' ')) {
-                    if (!word1.empty() && word1[0] == tolower(word1[0])) {
-                        word1[0] = toupper(word1[0]);
-                    }
-                    if (hasPunct(word)){
-                        continue;
-                    } else {
-                        f2 << word1 << " ";
-                    }
-                }
-                if (word[word.length() - 1] == '.' || word[word.length() - 1] == ',' || word[word.length() - 1] == '?' || word[word.length() - 1] == ';') {
-                    word.erase(word.length() - 1);
-                    if (hasPunct(word)){
-                        continue;
-                    }
-                    f2 << word << " ";
-
-            }
-
-
-            }
-            if (hasPunct(word)){
-                continue;
-            }
-        }
-    } else if (hasPunct(word) && !wordisPunct(word)) {
-        while (word[word.length() - 1] == '.' || word[word.length() - 1] == ',' || word[word.length() - 1] == '?' ||
-               word[word.length() - 1] == ';') {
-            word.erase(word.length() - 1);
-            if (hasPunct(word)) {
-                continue;
-            }
-            if (word[0] == tolower(word[0])) {
-                word[0] = toupper(word[0]);
-                f2 << word << " ";
-            } else {
-                f2 << word << " ";
-            }
-        }
-    }else if (word[0] == tolower(word[0]) && !hasPunct(word)) {
-        word[0] = toupper(word[0]);
-        f2 << word << " ";
-    } else if (word[0] == tolower(word[0]) && hasPunct(word) && !wordisPunct(word)) {
-        word[0] = toupper(word[0]);
-        word.erase(word.length() - 1);
-        f2 << word << " ";
-
-    } else if (word[0] == ',' || word[0] == '.' || word[0] == '?' || word[0] == ';') {
-        word.erase(0, 1);
-        f2 << word;
-    } else {
-        f2 << word << " ";
-    }
-}
-
-
 int main() {
-    fstream f1;
-    fstream f2;
+    fstream f1, temp, f2;
     f1.open("/home/pt/CLionProjects/LR7/f1.txt", ios::in);
-    f2.open("/home/pt/CLionProjects/LR7/f2.txt", ios::out);
+    temp.open("/home/pt/CLionProjects/LR7/temp.txt", ios::out);
     if (!f1) {
-        cout << "File not found!" << endl;
-        return -1;
+        cout << "File not found";
+        return 0;
     }
-    string word;
-    while (f1 >> word) {
-        processWord(word, f2);
+    char c;
+    bool wasSpace = true;
+    while (f1.get(c)) {
+        if (c == ' ') {
+            if (!wasSpace) {
+                temp << c;
+            }
+            wasSpace = true;
+        } else if (c == '.' || c == ',' || c == '?' || c == ';') {
+            temp << ' ';
+            wasSpace = true;
+        } else {
+            if (wasSpace) {
+                c = toupper(c);
+            }
+            temp << c;
+            wasSpace = false;
+        }
     }
+
     f1.close();
-    f2.close();
+    temp.close();
+    temp.open("/home/pt/CLionProjects/LR7/temp.txt", ios::in);
+    f2.open("/home/pt/CLionProjects/LR7/f2.txt", ios::out);
+    string word;
+    while (temp >> word) {
+        f2 << word << " ";
+    }
+    temp.close();
+
     return 0;
 }
-//govno!!!!!!!!!!!!!!!
